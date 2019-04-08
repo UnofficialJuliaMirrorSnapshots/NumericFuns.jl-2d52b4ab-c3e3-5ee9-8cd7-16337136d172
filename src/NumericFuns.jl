0176b6febf1e543d1a735f6d151305f77a410ca3
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 00:50:26 +0000

    Snapshot of branch master taken on 2019-04-07T20:50:25.859-04:00 from "https://github.com/UnofficialJuliaMirror/NumericFuns.jl-2d52b4ab-c3e3-5ee9-8cd7-16337136d172"

---
+++ src/NumericFuns.jl
@@ -0,0 +1,25 @@
+module NumericFuns
+
+    using Compat
+
+    export
+
+    # mathfuns
+    sqr, rcp, rsqrt, rcbrt, xlogx, xlogy,
+    sigmoid, logistic, logit, softplus, invsoftplus, logsumexp,
+
+    # functors (Note: export of specific functors are in functors.jl)
+    Functor, UnaryFunctor, BinaryFunctor, TernaryFunctor,
+    evaluate, @functor1, @functor2,
+
+    # rtypes
+    fptype, arithtype, result_type
+
+
+    # source files
+
+    include("mathfuns.jl")
+    include("functors.jl")
+    include("rtypes.jl")
+
+end # module
